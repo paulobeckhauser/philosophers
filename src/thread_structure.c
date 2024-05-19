a80ec0b9725724445d31d079d2217507e34dba91
Paulo Ricardo Beckhauser De Araujo <pabeckha@c2r12s7.42wolfsburg.de>
2024-05-19 21:52:55 +0200

    Change in the thread structure

--- src/thread_structure.c
+++ src/thread_structure.c
@@ -6,7 +6,7 @@
 /*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 19:17:52 by pabeckha          #+#    #+#             */
-/*   Updated: 2024/05/19 19:20:04 by pabeckha         ###   ########.fr       */
+/*   Updated: 2024/05/19 21:52:25 by pabeckha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@ static void handle_thread_error(int status, t_opcode opcode)
 {
     if (status == 0)
         return ;
+    if (status == EINVAL && (opcode == LOCK || opcode == UNLOCK))
+        ft_error("The value specifier by mutex is invalid.\n");
+    else if (status == EINVAL && opcode == INIT)
+        ft_error("The value specified by attr is invalid\n");
+    else if (status == EDEADLK)
+        ft_error("A deadlock would occur if the thread blocked waiting for mutex\n");
     
 }
 
